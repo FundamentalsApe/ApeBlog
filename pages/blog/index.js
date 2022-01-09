a85b8cf2b3c8a059e8dad94a690f4e9554eac20a
Vaibhav Sharma <vaibhav.sharma@soroco.com>
2022-01-09 12:13:55 +0530

    made changes for home and blog pages

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,13 +1,55 @@
 import Head from "next/head";
+import NavBar from "../../components/navBar"
+import styles from "../../styles/Blog.module.css"
 
 export default function Blog() {
     return (
-        <div>
+        <div className={styles.container}>
           <Head>
             <title>Ape's Blog</title>
             <link rel="icon" href="monkey.png" />
-
           </Head>
+          <div className={styles.listContainer}>
+            <div  className={styles.blogTitleContainer}>
+              <h2 className={styles.blogTitle}>
+                Blog Posts
+              </h2>
+              <div className={styles.titleUnderline}>
+              </div>
+            </div>
+            <div className={styles.listItem}>
+              <text className={styles.date}>
+                12 March 2021
+              </text>
+              <a href='/About' className={styles.postTitle}>
+                An introduction to DeFi
+              </a>
+            </div>
+            <div className={styles.listItem}>
+              <text className={styles.date}>
+                12 March 2021
+              </text>
+               <text className={styles.postTitle}>
+                  What is an AMM
+               </text>
+            </div>
+            <div className={styles.listItem}>
+              <text className={styles.date}>
+                12 March 2021
+              </text>
+              <text className={styles.postTitle}>
+                How to do business
+              </text>
+            </div>
+            <div className={styles.listItem}>
+              <text className={styles.date}>
+                12 March 2021
+              </text>
+              <text className={styles.postTitle}>
+                Where am I?
+              </text>
+            </div>
+          </div>
         </div>
       );
 }
